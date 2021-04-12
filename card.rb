aef02d3cb8fe8c6f9f793a0a6d756526669e4c51
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-12 16:44:41 +0500

    change hash in Card

--- card.rb
+++ card.rb
@@ -3,7 +3,7 @@
 class Card
   SUITS = %W[\u2660 \u2663 \u2666 \u2665].freeze
   RANKS = {
-    'A': 1,
+    A: 1,
     '2': 2,
     '3': 3,
     '4': 4,
@@ -13,9 +13,9 @@ class Card
     '8': 8,
     '9': 9,
     '10': 10,
-    'J': 2,
-    'Q': 3,
-    'K': 4
+    J: 2,
+    Q: 3,
+    K: 4
   }.freeze
 
   def initialize(rank, suit)
