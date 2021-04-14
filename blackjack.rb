3fa51584d9adeb9da9152c7e2ba3158a96b68b2f
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-14 13:12:16 +0500

    fix

--- blackjack.rb
+++ blackjack.rb
@@ -17,10 +17,11 @@ class BlackJack
   def preparation
     @ui.show_information('Welcolme to Blackjack for TN!!!')
     player_name = @ui.player_intro
+    @ui.show_information("Hello #{player_name}")
     @user = Player.new(player_name, PLAYER_BANK)
     @dealer = Player.new('Dealer', PLAYER_BANK)
     deck = Deck.new
-    deck.shuffle
+    deck.shuffle!
     @ui.show_information('The deck is shuffled')
   end
 
